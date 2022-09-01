74c9e38e3df24a92a27fa198d3c73c04f4b5cbdb
mamasidikova <sevara.mamasiidkova@mail.ru>
2022-09-01 22:45:24 +0300

    deploy_added

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = [
+    'sevarka.hopto.org',
+    '51.250.107.75',
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
+]
 
 
 # Application definition
